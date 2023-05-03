ad7e6afe684e34a88617f5f2986d01d9494ec1dc
Koro0 <ledane92@gmail.com>
2023-05-03 22:21:51 +0200

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+
+/**
+ * header
+ */
+import Navbar from './components/header/Navbar';
+import Basket from './components/header/Basket';
+import Search from './components/header/Search';
+
+/**
+ * categories
+ */
+import Categories from './components/Slide/Categories';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<IndexPage />} />
-    </Routes>
+      <header className='header'>
+        <Basket />
+        <Search />
+        <Navbar />
+      </header>
+      <main>
+        <Categories />
+        <Routes>
+          <Route path='/' element={<IndexPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
