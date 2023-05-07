291803d6111aba4b66b139b9883288723963c9d6
Koro0 <ledane92@gmail.com>
2023-05-08 00:12:28 +0200

    create content navbar

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function Navbar() {
+
   return (
     <>
-        nav
+        <nav className='header_navbar'>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+          
+        </nav>
     </>
   )
 }
