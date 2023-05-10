bdd9a069525deb562eba7b2543f59a45b47f5520
Koro0 <ledane92@gmail.com>
2023-05-10 20:58:44 +0200

    add ProductsList

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import ProductsList from "../components/ProductsList"
 
 export default function IndexPage() {
+
   return (
     <>
-        
+      <h1>All Products : </h1>
+        <ProductsList />
     </>
   )
 }
